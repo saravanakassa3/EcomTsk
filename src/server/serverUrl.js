4100c05f830e04169b4dea0a831444eed182cf28
saravanakassa3 <saravanakassa3@gmail.com>
2024-11-10 09:52:35 +0530

    Update serverUrl.js

--- src/server/serverUrl.js
+++ src/server/serverUrl.js
@@ -1,9 +1,9 @@
 export const ServerURL = {    
     //PRODUCTION_HOST_URL: `http://localhost:44301/`,   
-    PRODUCTION_HOST_URL: `https://ecommerceapi.kassapos.co.in/`,
+    PRODUCTION_HOST_URL: `https://ecomapi.kassapos.co.in/`,
     //PRODUCTION_HOST_URL:  `http://13.200.71.164:9002/`, 
     COMPANY_NAME: `E-Commerce Website`,
-    COMPANY_REF_ID: 41,
+    COMPANY_REF_ID: 45,
     CURRENCY: 'INR',
     MAP_KEY: 'AIzaSyC966V9u3Yu_TyIbqAiB4ukRNPBS5AWjhc'
 };
